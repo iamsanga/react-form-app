88c260067aa0b15022ae21e05931e1e569c661f3
iamsanga <iamsanga7@gmail.com>
2020-04-19 00:53:38 +0530

    Updates

---
+++ precache-manifest.a5523374013e349b6e5def02d69f7ff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc07c421b3511719970e",
+    "url": "/react-form-app/static/js/main.fc07c421.chunk.js"
+  },
+  {
+    "revision": "f5aeeb6c7ecb2c43a21a",
+    "url": "/react-form-app/static/css/1.b0fb2b19.chunk.css"
+  },
+  {
+    "revision": "f5aeeb6c7ecb2c43a21a",
+    "url": "/react-form-app/static/js/1.f5aeeb6c.chunk.js"
+  },
+  {
+    "revision": "c890866122e9b5d2e884",
+    "url": "/react-form-app/static/js/runtime~main.c8908661.js"
+  },
+  {
+    "revision": "70ddd3d62761c7d9d702020f277a48d0",
+    "url": "/react-form-app/index.html"
+  }
+];
