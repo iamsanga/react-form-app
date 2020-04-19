bda7a7d2879100f023a55f4316edc2c4234bde0a
iamsanga <iamsanga7@gmail.com>
2020-04-19 14:18:03 +0530

    Updates

---
+++ precache-manifest.2a97f1f0c0df02cbd9a95592fa1e0a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a3154fb6b020fb90563",
+    "url": "/react-form-app/static/js/main.7a3154fb.chunk.js"
+  },
+  {
+    "revision": "f5aeeb6c7ecb2c43a21a",
+    "url": "/react-form-app/static/css/1.b0fb2b19.chunk.css"
+  },
+  {
+    "revision": "f5aeeb6c7ecb2c43a21a",
+    "url": "/react-form-app/static/js/1.f5aeeb6c.chunk.js"
+  },
+  {
+    "revision": "c890866122e9b5d2e884",
+    "url": "/react-form-app/static/js/runtime~main.c8908661.js"
+  },
+  {
+    "revision": "0665058df3920dbe7e17bb72ef74b003",
+    "url": "/react-form-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-form-app/precache-manifest.ad730e0f91d29cb15d8e5083d9f4ed87.js"
+  "/react-form-app/precache-manifest.2a97f1f0c0df02cbd9a95592fa1e0a7a.js"
 );
 
 workbox.clientsClaim();
